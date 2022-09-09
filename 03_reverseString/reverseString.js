683d763c2bfc57b127f59e58a7d46673757ad2d8
wtucker <wtbtucker@gmail.com>
2022-09-09 14:11:18 -0400

    Reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let string_array = string.split("");
+    let array_reversed = string_array.reverse();
+    let string_reversed = array_reversed.join("");
+    return string_reversed;
 };
 
 // Do not edit below this line
