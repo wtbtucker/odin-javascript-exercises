f743912e8810a3a0cf6465888fb536ee7feef3fd
wtucker <wtbtucker@gmail.com>
2022-09-09 16:16:42 -0400

    Sum values between arguments inclusively

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let start;
+    let end;
+    let sum = 0;
+    if (!(a>=0 && b>=0) || typeof a !== "number" || typeof b !== "number"){
+        return 'ERROR';
+    }
+    if (b > a){
+        start = a;
+        end = b;
+    } else {
+        start = b;
+        end = a;
+    }
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
