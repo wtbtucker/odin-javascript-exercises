1eb9de5e5731f26eedf480097de57d200013691d
wtucker <wtbtucker@gmail.com>
2022-09-09 11:57:38 -0400

    Create function to repeat string num times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    let output_string = "";
+    for (let i = 0; i < num; i++) {
+        output_string += string;
+    }
+    return output_string;
 };
 
 // Do not edit below this line
