57b4ac9ee045c1198e3e16d1702f6d31fd1483ab
wtucker <wtbtucker@gmail.com>
2022-09-09 16:44:54 -0400

    Select leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    switch(true) {
+        case (year%400 === 0):
+            return true;
+        case (year%100 === 0):
+            return false;
+        case (year%4 === 0):
+            return true;
+        default:
+            return false;
+    }
 };
 
 // Do not edit below this line
