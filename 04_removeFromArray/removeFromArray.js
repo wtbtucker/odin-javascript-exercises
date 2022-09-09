5f332edf212be71c9e0e74669315e4a93a0035ad
wtucker <wtbtucker@gmail.com>
2022-09-09 15:28:51 -0400

    Remove arguments from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
-
+    const args = Array.from(arguments)
+    let original_array = args[0];
+    console.log(original_array);
+    // TODO: list of arguments that are present in the original array
+    for (let i=1; i < arguments.length; i++) {
+        console.log(args[i])
+        if (original_array.includes(args[i])) {
+            const j = original_array.indexOf(args[i]);
+            original_array.splice(j, 1);
+        } else {
+            continue;
+        }
+    }
+    return original_array;
 };
 
 // Do not edit below this line
