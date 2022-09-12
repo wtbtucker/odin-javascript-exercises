d280f80e923a517bbd9b8e1b8b5c112224a7dae2
wtucker <wtbtucker@gmail.com>
2022-09-12 16:33:29 -0400

    Working add, subtract, sum

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,19 @@
-const add = function() {
-	
+const add = function(a,b) {
+	let c = Number(a) + Number(b);
+  return c;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return Number(a) - Number(b);
 };
 
 const sum = function() {
-	
+  let sum = 0;
+  let number_array = arguments[0];
+	for (let i = 0; i < number_array.length; i++) {
+    sum += Number(number_array[i]);
+  }
+  return sum;
 };
 
 const multiply = function() {
