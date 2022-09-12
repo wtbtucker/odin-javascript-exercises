deac4272455fe08ab8ff8db9f18b3c38e83c0f03
wtucker <wtbtucker@gmail.com>
2022-09-12 17:47:17 -0400

    Add functionality for multiple, power, and factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -16,16 +16,29 @@ const sum = function() {
   return sum;
 };
 
+// check solution on this one
 const multiply = function() {
-
+  let product = 1;
+  let number_array = arguments[0];
+  console.log(number_array);
+  for (let i = 0; i < number_array.length; i++) {
+    let item = Number(number_array[i]);
+    product = product * item;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Number(a)**Number(b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial_product = 1;
+  for (let i = a; i > 0; i--) {
+    factorial_product *= i;
+  }
+
+	return factorial_product;
 };
 
 // Do not edit below this line
