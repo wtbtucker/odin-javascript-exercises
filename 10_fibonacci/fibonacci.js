6d72f67c56daeee496ae08b1ea4be9ea8a204f2f
wtucker <wtbtucker@gmail.com>
2022-09-12 19:36:05 -0400

    Create an array of the fibonacci sequence up to specified member, pop last number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    member = Number(member);
+    let array = [1,1];
+    for (let i = 1; i < (member-1); i++) {
+        let sum = array[i] + array[i-1];
+        array.push(sum);
+    }
+    return (member > 0) ? array.pop() : "OOPS";
 
 };
 
