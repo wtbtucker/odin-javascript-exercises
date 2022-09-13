3c25df50115c2d0fd2a742aec2bfcf2c734423a9
wtucker <wtbtucker@gmail.com>
2022-09-13 10:36:29 -0400

    Add function to create new array with age as keys

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let group_by_age = people.reduce((storage, obj) => {
+        let death = (obj.yearOfDeath) ? obj.yearOfDeath : new Date().getFullYear();
+        let age = death - Number(obj.yearOfBirth);
+        storage[age] = storage[age] || [];
+        storage[age].push(obj);
+        return storage;
+    }, {});
+    return group_by_age[Object.keys(group_by_age).pop()].pop();
 };
 
-// Do not edit below this line
 module.exports = findTheOldest;
